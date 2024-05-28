f1655ee0c38977ba88d20c50bf207f0def90fe9e
Maryem125 <meriam.matoussi@edu.isetcom.tn>
2024-05-28 16:25:04 +0100

    new file:   .env 	modified:   script1.py 	renamed:    whois -> whois.py

--- script1.py
+++ script1.py
@@ -1,6 +1,5 @@
 from scapy.all import *
 import csv,json
-import joblib
 import netifaces
 import sys,os
 import subprocess,requests
@@ -16,12 +15,14 @@ import dns.exception
 from dotenv import load_dotenv
 import threading
 import time
+import tldextract
+
 
 
 #headers = {'Authorization': 'a050b96276f0646d380048e886dc9288'}
 
 load_dotenv()
-"""_________________________________________________MISP_________________________________________________"""
+"""________________________________________________MISP________________________________________________"""
 
 
 
@@ -50,7 +51,7 @@ def check_domain(domain):
     
 
 
-"_________________________________________________CONVERT TO CSV_________________________________________________"
+"________________________________________________CONVERT TO CSV________________________________________________"
 
 def save_to_csv(filename, data):
 
@@ -58,11 +59,11 @@ def save_to_csv(filename, data):
     with f:
         writer = csv.writer(f)
         for row in data:
-            writer.writerow(row[1])
+            writer.writerow(row)
 
 
 
-"_________________________________________________CONVERT TO JSON________________________________________________"
+"________________________________________________CONVERT TO JSON_______________________________________________"
 def save_to_json(rows,filename):
 
     donnees_json = json.dumps(rows, indent=4)  # Convertir la liste en une chaîne JSON
@@ -73,23 +74,57 @@ def save_to_json(rows,filename):
 
 
 "///////////////////////////////////////////////////MODEL/////////////////////////////////////////////////////////////"
-def make_prediction (len,size_avg,size_stdev,throughput):
+def make_prediction (length_without_tld, subdomain_count,entropy1,average_length):
     url = 'http://127.0.0.1:8000/predict'  # l'URL de l'endpoint
     data = {
-        "len": len,
-        "size_avg": size_avg ,
-        "size_stdev":size_stdev
+            "len": length_without_tld,
+            "subdomain_count": subdomain_count,
+            "entropy":entropy1,
+            "size_avg":average_length
             }
     response = requests.post(url, json=data)
     if response.status_code == 200:
-        print("Réponse reçue : ", response.json())  # Afficher la réponse JSON
+        print("Réponse reçue : ", response.json())        # Afficher la réponse JSON
+        return response.json()
     else:
         print("Erreur : ", response.status_code)    
     
     
-"____________________________________________________FIN MODEL_________________________________________________________"
+"___________________________________________________FIN MODEL________________________________________________________"
 
 
+def calculate_entropy(request):
+    # Compter la fréquence de chaque caractère dans la requête
+    char_freq = {}
+    for char in request:
+        if char in char_freq:
+            char_freq[char] += 1
+        else:
+            char_freq[char] = 1
+
+    # Calculer l'entropie en utilisant la formule de Shannon
+    entropy = 0
+    total_chars = len(request)
+    for freq in char_freq.values():
+        probability = freq / total_chars
+        entropy -= probability * math.log2(probability)
+
+    return entropy
+
+def remove_tld(domain):
+    # Retirer le point final si présent
+    domain = domain.rstrip('.')
+
+    parts = domain.split('.')
+    if len(parts) > 2:
+        # Joindre toutes les parties sauf les deux dernières (TLD et domaine principal)
+        return '.'.join(parts[:-2])
+    elif len(parts) == 2:
+        # Si le domaine a exactement deux parties, retourner seulement la première partie
+        return parts[0]
+    else:
+        # Si le domaine n'a qu'une seule partie, retourner le domaine tel quel
+        return domain
     
 
 
@@ -111,7 +146,13 @@ def check_NXDOMAIN(domain):
         print(f"DNS query failed: {e}")
     return False
 
-
+def uppercase_ratio_dns(request):
+ if not request: # Si la requête est vide
+   return 0
+ uppercase_count = sum(1 for char in request if char.isupper())
+ total_characters = len(request)
+ ratio = (uppercase_count / total_characters) * 100
+ return ratio
 
 
 
@@ -189,7 +230,14 @@ def nsanswers (packet, nscount):
                 nst='NAN'
         return nst
 def list_interfaces():
+    """
+    Returns a list of all the interfaces on the system.
 
+    Returns
+    -------
+    list
+        List of interface names (str)
+    """
     iface = []
     interfaces = netifaces.interfaces()
     for i in interfaces:
@@ -197,13 +245,28 @@ def list_interfaces():
     return iface
 
 def get_ip_address(domain):
-
     try:
         ip_address = socket.gethostbyname(domain)
        
     except socket.gaierror:
         ip_address='n/a'
     return ip_address
+total_length = 0
+num_requests = 0
+
+
+
+# Fonction pour calculer la taille moyenne
+def calculate_average_length(query_length):
+    global total_length, num_requests
+    total_length += query_length
+    num_requests += 1
+    return total_length / num_requests
+
+
+
+
+
 
     
 
@@ -217,21 +280,25 @@ def calculate_length_without_tld(request):
         # Vérifier si le domaine a plus de deux parties
         if len(parts) > 2:
         # Joindre toutes les parties sauf les deux dernières
-            return len('.'.join(parts[:-2]))
+            return len('.'.join(parts[:-1]))
             # Si le domaine a exactement deux parties, retourner la longueur de la première partie
         elif len(parts) == 2:
             return len == 0
     # Sinon, retourner la longueur du domaine
     return len('.'.join(parts[:-2]))
     
-
+def remove_last_dot(domaine):
+            last_dot_position=domaine.rfind('.')
+            if last_dot_position==-1:
+                return domaine
+            return domaine[:last_dot_position] + domaine[last_dot_position +1:]
 # Calculer la taille moyenne des requêtes DNS
-def calculate_average_size(lengths):
+"""def calculate_average_size(lengths):
     if lengths:
         average_size = sum(lengths) / len(lengths)
         return average_size
     else:
-        return 0
+        return 0"""
 def handle_packet(packet):
     rows=[]
     if DNS in packet and IP in packet:
@@ -242,11 +309,10 @@ def handle_packet(packet):
         data_len = len(packet[UDP].payload) if  UDP in packet else len(packet[TCP].payload) if TCP in packet else 'N/A'
         src_port = packet[UDP].sport if UDP in packet else packet[TCP].sport if TCP in packet else 'N/A'
         dst_port = packet[UDP].dport if UDP in packet else packet[TCP].dport if TCP in packet else 'N/A'
-    
         protocol= 'UDP'if  UDP in packet else 'TCP' if TCP in packet else 'N/A'
         query = packet[DNS].qd.qname.decode() if packet[DNS].qd else 'N/A'
         qr_flag = packet[DNS].qr
-        qr_type = 'Query' if qr_flag == 0 else 'Response'
+        qr_type = 'Query' if packet[DNS].qr == 0 else 'Response'
         nscount = packet[DNS].nscount
         ancount = packet[DNS].ancount
         qtype = packet[DNS].qd.qtype
@@ -254,15 +320,25 @@ def handle_packet(packet):
         nst=nsanswers(packet,nscount)
         ant=answers(packet,ancount) 
         length_without_tld=calculate_length_without_tld(query)
+        average_length=calculate_average_length(length_without_tld)
+        ratio=uppercase_ratio_dns(query)
+        entropy1= calculate_entropy(remove_tld(query)) if packet[DNS].qd else 'N/A'
+        dns_query = packet.getlayer(DNS).qd
+        domain_name = dns_query.qname.decode('utf-8')
+        extracted = tldextract.extract(domain_name)
+        tldd=(extracted.domain + '.'+ extracted.suffix)
+        subdomain = extracted.subdomain
 
-        average_size=calculate_average_size(dns_lengths)
-        #throughput=calculate_throughput(query)
-        #print (throughput)
-        #model=make_prediction(1,1,1,1)
-        #get_ip_address(query)
-        if validate_fqdn(query) == True or check_NXDOMAIN (query) == True or check_domain(query) == True:
+        if subdomain:
+            # Compter le nombre de sous-domaines
+            subdomain_count=len(subdomain.split('.'))
+        else :
+            subdomain_count=0 
+
+        modele = make_prediction(length_without_tld, subdomain_count,entropy1,average_length)
+        print(modele)
+        if validate_fqdn(query) == True or check_NXDOMAIN (domain_name) == True or check_domain(tldd) == True or modele==True:
             attack=True      
-            # Lancer script2.py dans un thread séparé
             thread = threading.Thread(target=launch_script2, args=(packet[IP].dst, packet[IP].src, query, protocol, src_port, dst_port))
             thread.start()
         else:
@@ -272,15 +348,19 @@ def handle_packet(packet):
         # Ajouter les nouvelles informations au DataFrame
         rows.append ({
             'attack': attack ,
+            'ratio':ratio,
             'Source IP': packet[IP].src,
-            'Destination IP': packet[IP].dst,
             'Source Port': src_port,
-            'Destination Port': packet[IP].dport,"dst_port"
+            'Destination IP': packet[IP].dst,
+            'Destination Port': dst_port,
+            'modele': 'true'if modele ==   True else 'false',
             'misp response': check_domain(query) ,
             'Query': query,
+            'subdomain_count':subdomain_count,
             'length_without_tld':length_without_tld if  packet.haslayer(DNS) and packet[DNS].qr == 0 else 'N/A',
             #'ip of thr query':ipadd,
-            'average_size': average_size,
+            'average_length': average_length  if  packet.haslayer(DNS) and packet[DNS].qr == 0 else 'N/A',
+            'average_size': average_length,
             'Protocol': protocol,
             'request_size': data_len,
             'QR Type': qr_type,
@@ -298,15 +378,21 @@ def handle_packet(packet):
             'nscount':nscount,
             'ancount':ancount,
             'arscunt':packet[DNS].arcount,
-            'entropy_score': entropy(list(str(query).encode('utf-8')))
+            'entropy_score': entropy1
         })       
-             
-          
+        """with open("output.csv", mode='a', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=rows[0].keys())
+            file_exists = os.path.isfile(file_path) and os.path.getsize(file_path) > 0
+
+            # Écrire l'en-tête seulement si le fichier n'existe pas déjà ou est vide
+            if not file_exists:
+                writer.writeheader()"""
+
+            #writer.writerows(rows) 
         json_filename= "json_file" + datetime.now().strftime('%Y-%m-%d') +'.json'
         save_to_json(rows, json_filename)
 
-        filename = "output11.csv"
-        #save_to_csv(filename, rows)
+        
     return rows
     
 
@@ -323,7 +409,7 @@ lock = threading.Lock()
 
 def launch_script2(dst, src, query, protocol, src_port, dst_port):
     args = [str(dst), str(src), str(query), str(protocol), str(src_port), str(dst_port)]
-    subprocess.call(["python", "/home/osboxes/Downloads/pfe-main/test2.py"] + args)
+    subprocess.call(["python", "/home/mariem/pfe/script2.py"] + args)
 
         
 
@@ -342,10 +428,8 @@ def start_live_capture(interface, port):
 
 def script_launch (attack,args):
         # Appel du deuxième script en passant les arguments
-        subprocess.call(["python", "/home/osboxes/Downloads/pfe-main/test2.py"] + args, shell=True)
+        subprocess.call(["python", "/home/iheb_bachouel/pfe/script2.py"] + args, shell=True)
 
-def lanche_fastapi () :
-    subprocess.run("python",'api.py',shell=True)
 
 
 
@@ -360,9 +444,7 @@ if __name__ == '__main__':
             data = handle_packet(sys.argv[1])
         else:
             print("The file doesn't exist. Please check the path.")  
-   
     else:
-       
             mode= input ("Please choose to analyze a \n - pcap_file analysis :1\n - real_time analysis :2 \n")
             if mode== "pcap_file"or mode =='1':
                 file_path = get_valid_path()
@@ -391,6 +473,5 @@ if __name__ == '__main__':
     end=time.time()
     temps=end-start
     print (temps)
-
 
 
